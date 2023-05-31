6da4b09da40ba61521e26298f15f98cc187a5392
Stesh_Dev <donstesh@gmail.com>
2023-06-01 01:02:10 +0300

    About Section CRUD

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,48 +2,92 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\About;
+use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $about = About::orderBy('id','desc');
-        return view('about', compact('about'));
+        $abouts = About::orderBy('id','desc')->paginate(5);
+        return view('about.index', compact('abouts'));
     }
+
     /**
-    * Display the specified resource.
-    *
-    * @param  \App\about  $about
-    * @return \Illuminate\Http\Response
-    */
-    public function show(About $about)
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        return view('about',compact('about'));
+        //
     }
+
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \App\about  $about
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(About $about)
+    {
+        return view('about.edit',compact('about'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, About $about)
     {
         $request->validate([
-            'pic' => 'required',
             'title' => 'required',
             'descption' => 'required',
         ]);
         
         $about->fill($request->post())->save();
 
-        return redirect()->route('about')->with('success','About Has Been updated successfully');
+        return redirect()->route('about.index')->with('success','Record Has Been updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -2,11 +2,42 @@
 
 @section('content')
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">About Section</h1>
+        <!-- Page Heading -->
+        <h1 class="h3 mb-4 text-gray-800">About Section</h1>
+                    @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th width="280px">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($abouts as $about)
+                    <tr>
+                        <td>{{ $about->title }}</td>
+                        <td>{{ $about->descption }}</td>
+                        <td>
+                            <form action="{{ route('about.destroy',$about->id) }}" method="Post">
+                                <a class="btn btn-primary" href="{{ route('about.edit',$about->id) }}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+            </tbody>
+        </table>
+        {!! $abouts->links() !!}
 
                 </div>
                 <!-- /.container-fluid -->
