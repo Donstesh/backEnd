0249af84f251477c22e93527c5f45910a0105308
Stesh_Dev <donstesh@gmail.com>
2023-06-01 00:07:17 +0300

    views crud

---
+++ resources/views/sync/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Sync section edit</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+@endsection
